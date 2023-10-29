ee55c964fe7da3bc62c25679b1113efee1f7e72b
tehila kahana <tehila.kh@gmail.com>
2023-10-29 17:53:10 +0200

    first commit

---
+++ color.jsx
@@ -0,0 +1,6 @@
+var colors = require('colors');
+//var dad_jokes=require('dady-jokes');
+    console.log('so cool'.trap.rainbow);
+    console.log('also cool'.trap);
+    console.log('colorfull'.rainbow);
+
